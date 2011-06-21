127f30c0416f66a46615d8685bb89dcf4b45e62d
Akira Ohnishi <s06206ao@gmail.com>
2011-06-21 12:27:43 +0900

    新しくタブを開いた時にもタブが今いるタブの右に行くようになっていたので右端に行くよう修正

--- allpinned.js
+++ allpinned.js
@@ -15,11 +15,9 @@ chrome.tabs.onSelectionChanged.addListener(function(tabId, selectInfo) {
   });
 });
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  makePinned(tab);
-});
-
 chrome.tabs.onCreated.addListener(function(tab) {
   makePinned(tab);
-  chrome.tabs.move(tab.id, {index: currentTab.index + 1});
+  if (tab.url !== "chrome://newtab/") {
+    chrome.tabs.move(tab.id, {index: currentTab.index + 1});
+  }
 });
