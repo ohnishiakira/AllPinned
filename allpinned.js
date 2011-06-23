d060ac9c4dd97322b6e81196e851959103582475
Akira Ohnishi <s06206ao@gmail.com>
2011-06-23 19:32:59 +0900

    chrome.history.*を使ってタブが開かれる位置が変になるのを修正しようと思ったけどなんかうまくいかないので一旦作業中断

--- allpinned.js
+++ allpinned.js
@@ -1,5 +1,5 @@
 function makePinned(tab) {
-  if (tab.pinned == false) {
+  if (tab.pinned === false) {
     chrome.tabs.update(tab.id, {pinned: true});
   }
 }
@@ -17,8 +17,28 @@ chrome.tabs.onSelectionChanged.addListener(function(tabId, selectInfo) {
 
 chrome.tabs.onCreated.addListener(function(tab) {
   makePinned(tab);
+  
   if (tab.url !== "chrome://newtab/") {
-    chrome.tabs.move(tab.id, {index: currentTab.index + 1});
+    chrome.history.getVisits({url: tab.url}, function(visitItems) {
+      console.log(visitItems);
+      if (visitItems.length === 0) {
+	return false;
+      }
+
+      var visitItem = visitItems[visitItems.length - 1];
+
+      console.log(visitItem.transition);
+
+      switch (visitItem.transition) {
+	case "link":
+	  chrome.tabs.move(tab.id, {index: currentTab.index + 1});
+	  break;
+	case "start_page":
+	  // a link opened from other application
+	default:
+	  break;
+      }
+    });
   }
 });
 
